17bb186ea656de94109a823c99dc33e303f2f89b
Sergey Startsev <sergey.starcev@imeretinka.ru>
2020-02-03 17:15:23 +0300

    begin

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Context from './context';
+import CardList from './components/CardList/CardList';
 
-function App() {
+const SHOW_CARDS = 3;
+
+const App = () => {
+  const [cards, setCards] = React.useState([
+    {id: 1, title: `Валет`, code: `Valet`},
+    {id: 2, title: `Валет`, code: `Valet`},
+    {id: 3, title: `Валет`, code: `Valet`},
+    {id: 4, title: `Дама`, code: `Dama`},
+    {id: 5, title: `Дама`, code: `Dama`},
+    {id: 6, title: `Дама`, code: `Dama`},
+    {id: 7, title: `Король`, code: `Korol`},
+    {id: 8, title: `Король`, code: `Korol`},
+    {id: 9, title: `Король`, code: `Korol`},
+    {id: 10, title: `Туз`, code: `Tyz`},
+    {id: 11, title: `Туз`, code: `Tyz`},
+    {id: 12, title: `Туз`, code: `Tyz`}
+  ]);
+
+  const shuffledCards = (arr) => {
+    return arr.sort(() => Math.random() - 0.5);
+  };
+  
+  const getThreeCards = (cards) => {
+    return cards.slice(0, SHOW_CARDS);
+  };
+
+  const updateCards = () => {
+    setCards([...shuffledCards(cards)]);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <h1>Blind Bandit</h1>
+
+      <CardList cards={getThreeCards(cards)}/>
+      <button onClick={updateCards}>Обновить!</button>
     </div>
   );
 }
